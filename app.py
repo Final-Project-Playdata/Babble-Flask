856bd9db8d578113fbca3c2eb22fecb296dd92ba
cha wanghyeon <chawanghyeon@gmail.com>
2021-04-20 09:04:21 +0900

    Done connect with vue

--- app.py
+++ app.py
@@ -4,16 +4,13 @@ from my_util.my_logger import my_logger
 from datetime import datetime
 from pathlib import Path
 import my_util.googleSTT as stt
-import os, time
+import os
+import time
 from io import StringIO
 import pathlib
 import requests
 from flask import send_file, make_response
 from flask_http_response import success, result, error
-
-
-
-
 
 # instantiate the app
 app = Flask(__name__)
@@ -21,24 +18,34 @@ app = Flask(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
+
+
 @app.route('/STT', methods=['POST'])
 def STT():
+    print(request)
     file = request.files.get('audio')
-    username = 'id121'
-    file_dir = str(time.strftime("C:/audio/"+ username +"/%Y/%m/%d/"))  #여기 경로 변경 필요
+    print(file)
+    print('===========================')
+
+    # if '..' in file.filename or 'audio/wav' != file.mimetype:
+    #     return 'error'
+
+    file_dir = str(time.strftime(
+        "C:/audiotest/" + file.filename + "/%Y/%m/%d/"))
+
     pathlib.Path(file_dir).mkdir(parents=True, exist_ok=True)
-    file_name = datetime.strftime(datetime.now(), (username+".%Y-%m-%dT%H-%M-%S.%f"))[:-3] + ".wav"
+
+    file_name = datetime.strftime(
+        datetime.now(), (file.filename + ".%Y-%m-%d-%H-%M-%S")) + ".wav"
+
     file.save(os.path.join(file_dir, file_name))
 
     result = stt.total_api(file_dir, file_name)
 
-    after_sound = file_dir + file_name[:-4] + '.mp3'
-    
-    response = make_response(send_file(after_sound))
-    response.headers['result'] = result
+    return result
 
-    return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=os.getenv('FLASK_RUN_PORT'),debug=os.getenv('FLASK_DEBUG'))
+    app.run(host='0.0.0.0', port=os.getenv(
+        'FLASK_RUN_PORT'), debug=os.getenv('FLASK_DEBUG'))
--- my_util/googleSTT.py
+++ my_util/googleSTT.py
@@ -1,23 +1,25 @@
+import requests
+import json
+import os
+import io
+import pathlib
+import time
+import base64
+import urllib3
+import numpy as np
+import speech_recognition as sr
+from flask import Flask, jsonify
+from pathlib import Path
+from datetime import datetime
 from google.cloud import speech
 from pydub import AudioSegment
-AudioSegment.converter = r"C:\ITStudy\STT\ffmpeg\bin\ffmpeg.exe"
-from datetime import datetime
-from pathlib import Path
+AudioSegment.converter = r"C:\\ITstudy\\12.project\\Babble-Flask\\ffmpeg\\bin\\ffmpeg.exe"
 
-from flask import Flask, jsonify
-import speech_recognition as sr
-import numpy as np
-import io, os
-import urllib3
-import json, requests
-import base64
-
-import time
-import pathlib
 
 def open_audio(file_dir, file_name):
     with io.open(file_dir + file_name, "rb") as f:
         return f.read()
+
 
 def sample_recognize(file_dir, file_name):
     client = speech.SpeechClient()
@@ -35,7 +37,8 @@ def sample_recognize(file_dir, file_name):
     audio = {"content": open_audio(file_dir, file_name)}
     response = client.recognize(config=config, audio=audio)
 
-    profanityfilter = ['시발', '씨발', '시발놈아', '씨발놈아', '존나', '지랄', '미친새끼', '새끼가', '병신', '개새끼야', '지랄이야', '씹새끼야']
+    profanityfilter = ['시발', '씨발', '시발놈아', '씨발놈아', '존나',
+                       '지랄', '미친새끼', '새끼가', '병신', '개새끼야', '지랄이야', '씹새끼야']
     timeline, swear_timeline, words, filter_words = [], [], [], []
     paragraph = ''
     filter_paragraph = ''
@@ -44,10 +47,12 @@ def sample_recognize(file_dir, file_name):
         alternative = result.alternatives[0]
         for word in alternative.words[1:]:
             timeline.append([
-                int(word.start_time.seconds * 1000 + word.start_time.nanos * (10**-6)),
-                int(word.end_time.seconds * 1000 + word.end_time.nanos * (10**-6))
+                int(word.start_time.seconds * 1000 +
+                    word.start_time.nanos * (10**-6)),
+                int(word.end_time.seconds * 1000 +
+                    word.end_time.nanos * (10**-6))
             ])
-            
+
             words.append(word.word)
             paragraph = " ".join(words)
 
@@ -55,15 +60,17 @@ def sample_recognize(file_dir, file_name):
                 filter_words.append('*')
                 filter_paragraph = " ".join(filter_words)
                 swear_timeline.append([
-                    int(word.start_time.seconds * 1000 + word.start_time.nanos * (10**-6)),
-                    int(word.end_time.seconds * 1000 + word.end_time.nanos * (10**-6))
+                    int(word.start_time.seconds * 1000 +
+                        word.start_time.nanos * (10**-6)),
+                    int(word.end_time.seconds * 1000 +
+                        word.end_time.nanos * (10**-6))
                 ])
             else:
                 filter_words.append(word.word)
                 filter_paragraph = " ".join(filter_words)
 
-
     return swear_timeline, paragraph, filter_paragraph
+
 
 def create_beep(duration):
     sps = 48000
@@ -76,13 +83,14 @@ def create_beep(duration):
     wf_int = np.int16(wf_quiet * 32767)
 
     beep = AudioSegment(
-        wf_int.tobytes(), 
+        wf_int.tobytes(),
         frame_rate=sps,
-        sample_width=wf_int.dtype.itemsize, 
+        sample_width=wf_int.dtype.itemsize,
         channels=1
     )
 
     return beep
+
 
 def saltlux_api(service_id, type_number, text):
     params = {
@@ -99,39 +107,50 @@ def saltlux_api(service_id, type_number, text):
         params['argument']['query'] = text
         result = saltlux_api_post(params)
         if type_number == '0':
-            return [ result['label'] ]
+            return [result['label']]
         else:
-            return [ word[1] for word in result['Result'] ]
+            return [word[1] for word in result['Result']]
     elif service_id == "00116013830":
         params['argument']['question'] = text
         result = saltlux_api_post(params)
-        return [ word['keyword'] for word in result['return_object']['keylists'] ]
+        return [word['keyword'] for word in result['return_object']['keylists']]
+
 
 def saltlux_api_post(params):
     url = 'http://svc.saltlux.ai:31781'
     headers = {'Content-Type': 'application/json; charset=utf-8'}
-    response = requests.post(url, headers = headers, data = json.dumps(params))
-    response_json = json.loads(response.content.decode("utf-8", errors='ignore'))
+    response = requests.post(url, headers=headers, data=json.dumps(params))
+    response_json = json.loads(
+        response.content.decode("utf-8", errors='ignore'))
     return response_json
 
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
+
+
 def total_api(file_dir, file_name):
-    swear_timeline, paragraph, filter_paragraph = sample_recognize(file_dir, file_name)
+    swear_timeline, paragraph, filter_paragraph = sample_recognize(
+        file_dir, file_name)
     sound = AudioSegment.from_wav(file_dir + file_name)
     beep = create_beep(duration=1030)
-  
+
     if swear_timeline:
         for i in range(len(swear_timeline)):
-            beep = create_beep(duration=swear_timeline[i][1] - swear_timeline[i][0])
-            sound = sound.overlay(beep, position=swear_timeline[i][0], gain_during_overlay=-20)
-    sound.export(file_dir + file_name[:-4] + '.mp3', format='mp3')
+            beep = create_beep(
+                duration=swear_timeline[i][1] - swear_timeline[i][0])
+            sound = sound.overlay(
+                beep, position=swear_timeline[i][0], gain_during_overlay=-20)
 
+    audio_name = file_dir + file_name[:-4] + '.mp3'
+    sound.export(audio_name, format='mp3')
+
+    print(audio_name)
     collection = {
+        'name': file_name[:-4] + '.mp3',
         'paragraph': paragraph,
-        'filter_paragraph' : filter_paragraph,
-        'sensitivity' : saltlux_api('11987300804', '0', paragraph),
-        'emotion' : saltlux_api('11987300804', '1', paragraph),
-        'keyword' : saltlux_api('00116013830', '1', filter_paragraph)
+        'filter_paragraph': filter_paragraph,
+        'sensitivity': saltlux_api('11987300804', '0', paragraph),
+        'emotion': saltlux_api('11987300804', '1', paragraph),
+        'keyword': saltlux_api('00116013830', '1', filter_paragraph)
     }
     result = json.dumps(collection, ensure_ascii=False).encode('utf-8')
 
